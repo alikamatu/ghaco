2f11c8c5f55f1b7f867450994b355a474429803a
alikamatu <122170675+alikamatu@users.noreply.github.com>
2024-05-20 06:55:40 +0000

    Add files via upload

--- src/components/Homepage/HomaPage.jsx
+++ src/components/Homepage/HomaPage.jsx
@@ -1,16 +1,10 @@
-import React, { useEffect } from 'react';
-import './Homepage.scss';
+import React from 'react';
+  import './Homepage.scss';
 import Image1 from '../photos/BACK.jpeg';
 import Image2 from '../photos/ago_downloaded.jpg';
 import Image3 from '../photos/1705809358963.jpeg';
 
 const Homepage = () => {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000, // Duration of animation in milliseconds
-      once: true // Whether animation should happen only once
-    });
-  }, []);
 
   return (
     <div className="homepage">
